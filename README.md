de93f656c66125d4f9eff9fd15174e17cc1672fd
Hamed Mohammadpour <hamedmp2012@gmail.com>
2018-01-14 22:26:06 +0100

    Readme complete.

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # SuperTabs
+---
+
+List and search through your open tabs with ease! 
+
+![supertabs_1400](screenshots/supertabs_1400.png)
+
+
+
+Familiar scene? No worries anymore. 
+
+![supertab](screenshots/supertab_3.png)
+
+
+
+## Introduction
+
+A superuser tool to acomplish your tasks and internet surfing faster. No more scrolling over tabs that you can't even read their titles. Easily list them and search them. 
+
+![supertab](screenshots/supertab.png)
+
+## Features
+
+- See number of open tabs across all windows as the badge counter ![badge](screenshots/badge.png)
+- List all open tabs
+- Search through titles and urls of the tabs and quickly switch them by clicking on them.
+
+## Permissions
+- Tabs permission: To access currently open tabs and their url and titles.
+
+No network or other permissions are used. 
+
+
+## FEEDBACK AND BUGS
+Please report all your valuable feedback, feature requests and bug reports on the github issues page.
+
+## Release notes
+
+- 2018.01.13 - v.0.0.1 - Initial release
+
+## ACKNOWLEDGEMENTS
+
+- Icon made by <a href="https://www.flaticon.com/authors/smashicons" title="Smashicons">Smashicons</a> 
---
+++ popup.js
@@ -0,0 +1,126 @@
+
+/*
+ Copyright (c) 2018, Hamed MP
+ @thehamedmp
+
+ All rights reserved.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of the author nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL EVAN JEHU BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+
+// Search the tabs when entering the search keyword.
+$(function() {
+  $('#search').keyup(function() {
+     $('#tabs').empty();
+     dumpTabs($('#search').val())
+  });
+});
+
+$(document).keypress(function(e) {
+    if(e.which == 13) {
+        
+    }
+});
+
+function dumpTabs(query){
+  var tabs = chrome.tabs.query({},function(tabs){
+    // console.log(tabs);
+    populateTabs(tabs, query);
+  });
+
+}
+
+function populateTabs(tabs, query){
+  $('#tabs').append(parseTabs(tabs, query));
+}
+
+function parseTabs(tabs, query){
+  var list = $('<ul>');
+  var i;
+  for (i = 0; i < tabs.length; i++) {
+    list.append(dumpNode(tabs[i], query));
+  }
+  return list;
+}
+
+function dumpNode(tab, query) {
+  if (tab.title) {
+    if (query && String(tab.title.toUpperCase() + tab.url.toUpperCase()).indexOf(query.toUpperCase()) == -1) {
+      return $('<span></span>');
+    }
+
+    var anchor = $('<a>');
+    anchor.attr('href', tab.url);
+    anchor.attr('style', 'margin-left: 15px; display:inline-block;');
+    anchor.text(tab.title.substring(0, 27) + ' ...');
+    /*
+     * When clicking on a tabs in the extension, a new tab is fired with
+     * its url.
+     */
+    anchor.click(function() {
+      chrome.tabs.update(tab.id, {active: true});
+      // chrome.tabs.create({url: tab.url});
+    });
+
+    var fav = $('<img>', {src: tab.favIconUrl ? tab.favIconUrl : 'assets/icon_32.png', 
+                          height:"32px", 
+                          width:"32px", 
+                          alt:""
+                          });
+    fav.attr('style', 'vertical-align:middle; height:32px, width:32px; max-width:32px; max-height:32px');
+
+    var span = $('<div>');
+    span.attr('style', ' display: flex;');
+
+    if (tab.highlighted){
+      anchor.css("background-color", "#f2f2f2");
+    }  else {
+      anchor.css("background-color", "#fff");
+    }
+    // anchor.attr('style', ' margin-right: 0px;');
+
+    var options = $('<span> [<a id="deletelink" ' +
+        'href="#">Close</a>]</span>');
+    var edit =  $('<input>');
+    // Show add and edit links when hover over.
+        span.hover(function() {
+        // span.append(options);
+        $('#deletelink').click(function() {
+          chrome.tabs.remove(String(tab.id));
+          span.parent().remove();
+          
+         });
+        
+        options.fadeIn();
+      },
+      // unhover
+      function() {
+        options.remove();
+      }).append(fav, anchor);
+  }
+  var li = $(tab.title ? '<li>' : '<div>').append(span);
+  return li;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  dumpTabs();
+});
